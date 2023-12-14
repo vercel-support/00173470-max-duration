a9697da42b7fd18a61473b261848d031828f7865
Riz <rizwana.akmal@hotmail.com>
2023-12-14 16:26:20 +0000

    route.ts

---
+++ app/api/serverless-example.tsx/route.ts
@@ -0,0 +1,17 @@
+export const maxDuration = 60;
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+export function GET(request: NextRequest) {
+  return NextResponse.json(
+    {
+      body: request.body,
+      path: request.nextUrl.pathname,
+      query: request.nextUrl.search,
+      cookies: request.cookies.getAll(),
+    },
+    {
+      status: 200,
+    }
+  );
+}
